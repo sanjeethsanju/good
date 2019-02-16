cd576c0c2fdbce9427b0fa5e9a7c144be4c59652
sanjeethsanju <47620776+sanjeethsanju@users.noreply.github.com>
2019-02-16 23:05:44 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,26 @@
+#include<stdio.h>
+float power(float x,int y)
+{
+    float temp;
+    if(y==0)
+    return 1;
+    temp=power(x,y/2);
+    if(y%2==0)
+    return temp*temp;
+    else
+    {
+        if(y>0)
+        return x*temp*temp;
+        else
+        return(temp*temp)/x;
+    }
+}
+int main()
+{
+    float x=2;
+    int y=3;
+    printf("%f",power(x,y));
+        return 0;
+}
+    
+    
